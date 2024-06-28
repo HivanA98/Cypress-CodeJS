47d0dd25bd327ace2ce444f48e5b7e3477767a31
Ivan A <hivanarmadi@gmail.com>
2024-06-28 07:35:50 +0700

    Cypress V.2.1 -Update.Herokuapp

--- HeroKuApp/cypress/e2e/HeroKuApp.cy.js
+++ HeroKuApp/cypress/e2e/HeroKuApp.cy.js
@@ -1,28 +1,50 @@
 /// <reference types="cypress" />
 
-describe('Test February', () => {
+describe('HeroKuApp Test', () => {
 
-  it('Just Login', () => {
+  it('Normal Login', () => {
     cy.visit('https://katalon-demo-cura.herokuapp.com')
       cy.get('#btn-make-appointment').click()
       cy.get('#txt-username').type('John Doe')
       cy.get('#txt-password').type('ThisIsNotAPassword')
       cy.get('#btn-login').click()
   });
+
+  it('False Login', () => {
+    cy.visit('https://katalon-demo-cura.herokuapp.com')
+      cy.get('#btn-make-appointment').click()
+      cy.get('#txt-username').type('John Doe')
+      cy.get('#txt-password').type('wrong')
+      cy.get('#btn-login').click()
+      cy.get('.lead.text-danger')
+  });
     
-  it('visit the website', () => {
+  it('E2E basic', () => {
       cy.visit('https://katalon-demo-cura.herokuapp.com')
       cy.get('#btn-make-appointment').click()
       cy.get('#txt-username').type('John Doe')
       cy.get('#txt-password').type('ThisIsNotAPassword')
       cy.get('#btn-login').click()
-      //cy.get('#combo_facility').click()
       cy.get('#chk_hospotal_readmission').click()
       cy.get('#txt_comment').click().type('Ini comment')
       cy.get('#txt_visit_date').type('22/10/2021')
-      //cy.get('#txt_comment').click().type('Ini comment')
       cy.get('#btn-book-appointment').click()
-      
+  });    
     
-    });
+  it('E2E Variation', () => {
+        cy.visit('https://katalon-demo-cura.herokuapp.com')
+        cy.get('#btn-make-appointment').click()
+        cy.get('#txt-username').type('John Doe')
+        cy.get('#txt-password').type('ThisIsNotAPassword')
+        cy.get('#btn-login').click()
+        cy.get('#combo_facility').select('Seoul CURA Healthcare Center')
+        cy.get('#txt_comment').type('Ini comment')
+        cy.get('#txt_comment').clear()
+        cy.get('#txt_comment').type('Ini comment baru')
+        cy.get('#txt_visit_date').type('22/10/2021')
+        cy.get('#btn-book-appointment').click()
+
+  });
+
+
   });
