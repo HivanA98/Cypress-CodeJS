adc3b834f0c4d3435bed2bb8702cb13e3f881e0b
Ivan A <hivanarmadi@gmail.com>
2024-07-04 10:58:03 +0700

    Cypress- V.2.3.AddNewTestCase-SauceDemo

--- Saucedemo/cypress/e2e/ErrorUser.cy.js
+++ Saucedemo/cypress/e2e/ErrorUser.cy.js
@@ -6,22 +6,19 @@ describe('my first test', () => {
         cy.visit('https://www.saucedemo.com/')
         
         cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username);
+            cy.get("#user-name").type(user.username4);
             cy.get("#password").type(user.password);
              })
     
         cy.get('#login-button').click()
         cy.get('#add-to-cart-sauce-labs-backpack').click()
         cy.get('#add-to-cart-sauce-labs-bike-light').click()
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
-        cy.get('#add-to-cart-sauce-labs-fleece-jacket').click()
         cy.get('#add-to-cart-sauce-labs-onesie').click()
         cy.get('.shopping_cart_link').click()
         cy.get('#checkout').click()
         cy.get('#first-name').type('a')
-        cy.get('#last-name').type('a')
         cy.get('#postal-code').type('a')
         cy.get('#continue').click()
-        cy.get('#finish').click()
+        cy.get('#finish').should('be.visible')
         });
     });
