f4c4b8e71965a1982dd42fef12a3a8951e00f866
Ivan A <hivanarmadi@gmail.com>
2024-07-16 08:44:59 +0700

    Cypress-UpdateConfig&SupportCommands

--- RahulAcademy/cypress.config.js
+++ RahulAcademy/cypress.config.js
@@ -1,12 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // implement node event listeners here
     },
   },
-})
+});
