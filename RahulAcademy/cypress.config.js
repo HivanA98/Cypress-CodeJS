29f9fa3cb8788b2f2da26d344706577616f9ebe2
Ivan A <hivanarmadi@gmail.com>
2024-07-19 16:08:38 +0700

    Revert "Cypress-UpdateConfig&SupportCommands"
    This reverts commit f4c4b8e71965a1982dd42fef12a3a8951e00f866.

--- RahulAcademy/cypress.config.js
+++ RahulAcademy/cypress.config.js
@@ -1,10 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on, config)
     },
   },
-});
+})
