2b7ea56e4ab12e31f29567e56e90ef61c7fd27a5
Ivan A <hivanarmadi@gmail.com>
2024-07-21 07:02:30 +0700

    Cypress- V.3.1.0-ImproveCode-SauceDemo

--- Saucedemo/cypress/e2e/Verify False Login.cy.js
+++ Saucedemo/cypress/e2e/Verify False Login.cy.js
@@ -1,16 +1,13 @@
 /// <reference types="cypress" />
+import steps from './Step'
 
-describe('my first test', () => {
+describe('False Password', () => {
     
     it('visit standard_user', () => {
-        cy.visit('https://www.saucedemo.com')
-        
-        cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username);
-            cy.get("#password").type(user.wrongpassword);
-             })
-    
-            cy.get('input#login-button.submit-button.btn_action').click()
-            cy.get('.error-message-container').should('be.visible')
+        steps.URL()
+        steps.NormalUsername()
+        steps.wrongPassword()
+        steps.BtnLogin()
+        steps.FailedLogin()
         });
     });
