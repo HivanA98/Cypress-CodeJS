428f549e3f13e02ae39e0a67a83c9ced512fcc70
Ivan A <hivanarmadi@gmail.com>
2024-07-21 11:29:40 +0700

    Cypress- V.3.0.2-ImproveSimpleCode

--- Saucedemo/cypress/support/e2e.js
+++ Saucedemo/cypress/support/e2e.js
@@ -15,6 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+before(() => {
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
