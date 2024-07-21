9ac8b91b90dc930d8b5e2ede8f682384ba99408c
Ivan A <hivanarmadi@gmail.com>
2024-07-21 11:31:23 +0700

    Cypress- V.3.3.0-ImproveSimpleCode

--- Saucedemo/cypress/e2e/All Account.cy.js
+++ Saucedemo/cypress/e2e/All Account.cy.js
@@ -1,77 +1,52 @@
 /// <reference types="cypress" />
+import steps from './Step'
 
-describe('my first test', () => {
+describe('All Account Login', () => {
     
     it('visit standard_user', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username);
-            cy.get("#password").type(user.password);
-             })
-        cy.get('#login-button').click()
-        cy.get('#add-to-cart-sauce-labs-backpack').click()
-        cy.get('#add-to-cart-sauce-labs-bike-light').click()
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
-        cy.get('#add-to-cart-sauce-labs-fleece-jacket').click()
-        cy.get('#add-to-cart-sauce-labs-onesie').click()
-        cy.get('.shopping_cart_link').click()
-        cy.get('#checkout').click()
-        cy.get('#first-name').type('Ivan')
-        cy.get('#last-name').type('A')
-        cy.get('#postal-code').type('202220')
-        cy.get('#continue').click()
-        cy.get('#finish').click()
+        steps.URL()
+        steps.NormalUsername()
+        steps.Password()
+        steps.BtnLogin()
+        steps.addItems()
+        steps.CartMenu()
+        steps.AddreasFirstName()
+        steps.AddreasLastName()
+        steps.AddreasPostalCode()
+        steps.Finish()
         });
 
     it('visit locked_out_user', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username1);
-            cy.get("#password").type(user.password);
-            })
-        cy.get('#login-button').click()
-        cy.get('.error-message-container').should('be.visible')
+        steps.URL()
+        steps.LockedUsername()
+        steps.Password()
+        steps.BtnLogin()
+        steps.LockedUser()
         });   
             
     it('visit problem_user', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username2);
-            cy.get("#password").type(user.password);
-            })
-        cy.get('#login-button').click()
-        cy.get('#add-to-cart-sauce-labs-backpack').click()
-        cy.get('#add-to-cart-sauce-labs-bike-light').click()
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
-        cy.get('#add-to-cart-sauce-labs-fleece-jacket').click()
-        cy.get('#add-to-cart-sauce-labs-onesie').click()
-        cy.get('.shopping_cart_link').click()
-        cy.get('#checkout').click()
-        cy.get('#first-name').type('a')
-        cy.get('#postal-code').type('a')
-        cy.get('#continue').click()
-        cy.get('.error-message-container').should('be.visible')
+        steps.URL()
+        steps.ProblemUsername()
+        steps.Password()
+        steps.BtnLogin()
+        steps.addItems()
+        steps.CartMenu()
+        steps.AddreasFirstName()
+        steps.AddreasPostalCode()
+        steps.errorProblemUser
         });
                 
-
     it('visit performance_glitch_user', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username3);
-            cy.get("#password").type(user.password);
-            })
-        cy.get('#login-button').click()
-        cy.get('#add-to-cart-sauce-labs-backpack').click()
-        cy.get('#add-to-cart-sauce-labs-bike-light').click()
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
-        cy.get('#add-to-cart-sauce-labs-fleece-jacket').click()
-        cy.get('#add-to-cart-sauce-labs-onesie').click()
-        cy.get('.shopping_cart_link').click()
-        cy.get('#checkout').click()
-        cy.get('#first-name').type('a')
-        cy.get('#last-name').type('a')
-        cy.get('#postal-code').type('a')
-        cy.get('#continue').click()
-        cy.get('#finish').click()
+        steps.URL()
+        steps.PerformanceUsername()
+        steps.Password()
+        steps.BtnLogin()
+        cy.wait(5000)
+        steps.addItems()
+        steps.CartMenu()
+        steps.AddreasFirstName()
+        steps.AddreasLastName()
+        steps.AddreasPostalCode()
+        steps.Finish()
         });
     }); 
