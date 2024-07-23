be58e30978b5a1f80e4bfaa6201a74b803200aa6
Ivan A <hivanarmadi@gmail.com>
2024-07-23 17:32:23 +0700

    HerokuApp - V.3.0.0-ImproveSimpleCode

--- HeroKuApp/cypress/support/e2e.js
+++ HeroKuApp/cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+before(() => {
+    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+});
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
