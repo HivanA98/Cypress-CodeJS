ad15d1bafc9724ef6342f829fb19304fecf7d2f7
Ivan A <hivanarmadi@gmail.com>
2024-08-02 19:44:31 +0700

    Cypress- V.3.4.0-ImproveSimpleCode

--- Saucedemo/cypress/e2e/E2E.cy.js
+++ Saucedemo/cypress/e2e/E2E.cy.js
@@ -1,27 +1,18 @@
 /// <reference types="cypress" />
+import steps from './Step'
 
 describe('my first test', () => {
     
     it('visit standard_user', () => {
-        cy.visit('https://www.saucedemo.com/')
-        
-        cy.fixture("user").then(user => {
-            cy.get("#user-name").type(user.username);
-            cy.get("#password").type(user.password);
-             })
-    
-        cy.get('#login-button').click()
-        cy.get('#add-to-cart-sauce-labs-backpack').click()
-        cy.get('#add-to-cart-sauce-labs-bike-light').click()
-        cy.get('#add-to-cart-sauce-labs-bolt-t-shirt').click()
-        cy.get('#add-to-cart-sauce-labs-fleece-jacket').click()
-        cy.get('#add-to-cart-sauce-labs-onesie').click()
-        cy.get('.shopping_cart_link').click()
-        cy.get('#checkout').click()
-        cy.get('#first-name').type('a')
-        cy.get('#last-name').type('a')
-        cy.get('#postal-code').type('a')
-        cy.get('#continue').click()
-        cy.get('#finish').click()
+        steps.URL()
+        steps.NormalUsername()
+        steps.Password()
+        steps.BtnLogin()
+        steps.addItems()
+        steps.CartMenu()
+        steps.AddreasFirstName()
+        steps.AddreasLastName()
+        steps.AddreasPostalCode()
+        steps.Finish()
         });
-    });
+        });
