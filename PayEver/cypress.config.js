8bac4d57691662cb3171addc8fbc6da034e9288f
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-08-07 19:44:10 +0700

    PayEver-Update Config

--- PayEver/cypress.config.js
+++ PayEver/cypress.config.js
@@ -3,8 +3,20 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
+    baseUrl: "https://commerceos.staging.devpayever.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
 });
