a9550292bd1f2bc4b2fd5ec3aea3aa02b80c7ba4
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-08-07 19:41:54 +0700

    HerokuApp-Update Config

--- HeroKuApp/cypress.config.js
+++ HeroKuApp/cypress.config.js
@@ -2,8 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://katalon-demo-cura.herokuapp.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
 });
