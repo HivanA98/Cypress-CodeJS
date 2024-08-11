0af6acafbc93d6f59b6bb05b3751c2490391b93a
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2024-08-11 18:58:18 +0700

    BeautyHaul-BDD-Draft

---
+++ BeautyHaul(BDD)/cypress.config.js
@@ -0,0 +1,38 @@
+const { defineConfig } = require("cypress");
+
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents (on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on('file:preprocessor', browserify.default(config));
+
+  return config;
+}
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  chromeWebSecurity: false,
+  video: false,
+  pageLoadTimeout: 100000,
+  defaultCommandTimeout: 100000,
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.feature',
+    supportFile: false,
+    baseUrl: "https://www.beautyhaul.com",
+    stepDefinitions: 'cypress/stepsDefinition',
+    setupNodeEvents
+      
+  },
+
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  }
+});
