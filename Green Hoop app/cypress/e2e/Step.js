1cb080e57d61ff86d5f357c1ccbd8cba080519e4
Ivan Armadi <103761586+HivanA98@users.noreply.github.com>
2025-04-08 22:16:04 +0700

    GHP-Cypress-V.2.0.0-Login with all account

--- Green Hoop app/cypress/e2e/Step.js
+++ Green Hoop app/cypress/e2e/Step.js
@@ -2,53 +2,57 @@
 
 ///Login
 const website = "https://uat.greenhoopapp.com/"
-const InpUsername = ":r0:"
-const InpPassword = "#:r1:"
-const BtnLogin = "astd-login-btn"
-
-const 
-
-
+const InpUsername = '[id=":r0:"]'
+const InpPassword = '[id=":r1:"]'
+const BtnLogin = '[id=":r2:"]'
 
 class Main {
     static URL(){
         cy.visit(website)
     }
     
-   static astdaAdmin(){    
+   static astdAdmin(){    
     cy.fixture("account").then(account => {
         cy.get(InpUsername).type(account.astd)
-        cy.get(InpUsername).should('have.value', account.astdP)
+        cy.get(InpUsername).should('have.value', account.astd)
+        cy.get(InpPassword).type(account.astdP)
+        cy.get(InpPassword).should('have.value', account.astdP)
         })}
 
    static collectorAdmin(){    
     cy.fixture("account").then(account => {
         cy.get(InpUsername).type(account.collectoradmin)
-        cy.get(InpUsername).should('have.value', account.collectoradminP)
+        cy.get(InpUsername).should('have.value', account.collectoradmin)
+        cy.get(InpPassword).type(account.collectoradminP)
+        cy.get(InpPassword).should('have.value', account.collectoradminP)
         })}
       
    static manufaturAdmin(){    
     cy.fixture("account").then(account => {
         cy.get(InpUsername).type(account.manufacturadmin)
-        cy.get(InpUsername).should('have.value', account.manufacturadminP)
+        cy.get(InpUsername).should('have.value', account.manufacturadmin)
+        cy.get(InpPassword).type(account.manufacturadminP)
+        cy.get(InpPassword).should('have.value', account.manufacturadminP)
         })}
         
    static logisticAdmin(){    
     cy.fixture("account").then(account => {
         cy.get(InpUsername).type(account.logisticadmin)
-        cy.get(InpUsername).should('have.value', account.logisticadminP)
+        cy.get(InpUsername).should('have.value', account.logisticadmin)
+        cy.get(InpPassword).type(account.logisticadminP)
+        cy.get(InpPassword).should('have.value', account.logisticadminP)
         })}
 
    static customerAdmin(){    
     cy.fixture("account").then(account => {
         cy.get(InpUsername).type(account.customeradmin)
-        cy.get(InpUsername).should('have.value', account.customeradminP)
+        cy.get(InpUsername).should('have.value', account.customeradmin)
+        cy.get(InpPassword).type(account.customeradminP)
+        cy.get(InpPassword).should('have.value', account.customeradminP)
         })}
 
     static Login(){    
     cy.get(BtnLogin).should('be.visible').and('not.be.disabled').click()
         }
-
-
     }
 export default Main;
--- Green Hoop app/cypress/e2e/login.cy.js
+++ Green Hoop app/cypress/e2e/login.cy.js
@@ -4,8 +4,32 @@ import steps from './Step.js'
 describe('my first test', () => {
     
     it('astd login', () => {
-        steps.WEB()
-        steps.astdaAdmin()
-        steps.Login()
+        steps.URL()
+        steps.astdAdmin()
+        // steps.Login()
+    })
+
+    it('collector login', () => {
+        steps.URL()
+        steps.collectorAdmin()
+        // steps.Login()
+    })
+
+    it('manufactur login', () => {
+        steps.URL()
+        steps.manufaturAdmin()
+        // steps.Login()
+    })
+
+    it('logistic login', () => {
+        steps.URL()
+        steps.logisticAdmin()
+        // steps.Login()
+    })
+
+    it('customer login', () => {
+        steps.URL()
+        steps.customerAdmin()
+        // steps.Login()
     })
 })
